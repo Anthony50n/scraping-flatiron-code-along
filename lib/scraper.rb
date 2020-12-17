0f4880eaf9211d67ae362e13624117bfa2211f6f
Anthony50n <Anthony50n@yahoo.com>
2020-12-16 21:56:02 -0500

    commit tree 6ff0ab3632170a6ade91dec7b2c467e3e288a240

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,28 @@ class Scraper
       end
     end
   end
+
+  
+  def get_page
+    html = open("http://flatironschool.com/courses")
+    @doc = Nokogiri::HTML(html)
+  end
+
+  def get_courses
+   @course_info =  get_page.css('.post')
+
+  end
+
+  def make_courses
+    get_courses.each do |course|
+       c = Course.new
+       c.title = course.children.children.first.text
+       c.schedule = course.children.children[1].text
+       c.description = course.children.children[2].text
+       Course.all << c
+     end
+    
+  end
   
 end
 
